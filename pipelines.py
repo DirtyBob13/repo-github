eda127d15d2a66284c56d34a0eea3c697a2462c8
DirtyBob13 <89256890+DirtyBob13@users.noreply.github.com>
2022-04-22 12:31:05 +0300

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -5,12 +5,13 @@
 
 # useful for handling different item types with a single interface
 
-from itemadapter import ItemAdapter
-from pymongo import MongoClient
-from scrapy.pipelines.images import ImagesPipeline
+import re
 import scrapy
-import os
-from urllib.parse import urlparse
+import hashlib
+from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
 class JobparserPipeline:
     def __init__(self):
@@ -20,8 +21,41 @@ class JobparserPipeline:
 
     def process_item(self, item, spider):
         collection = self.mongobase[spider.name]
-        collection.insert_one(item)
+        item['characteristics'] = self.process_characteristics(item)
+        item['_id'] = item['photos'][0]['checksum']
+
+        try:
+            collection.insert_one(item)
+        except DuplicateKeyError:
+            None
+
         return item
+
+    def process_characteristics(self, item):
+        # Характеристики будем складывать в список списков, где в каждом
+        # подсписке первым значением будет название характеристики, а
+        # вторым - сама характеристика
+        char_df = []
+
+        flag = True
+        for line in item['characteristics']:
+            if re.findall(r'\w', line):
+                # Если это возможно, приведем значение к типу int
+                try:
+                    inp = int(re.search(r'\b.*\b', line)[0])
+                except ValueError:
+                    # Теперь попробуем к float
+                    try:
+                        inp = float(re.search(r'\b.*\b', line)[0])
+                    except ValueError:
+                        inp = re.search(r'\b.*\b', line)[0]
+
+                if flag:
+                    char_df.append([inp])
+                    flag = False
+                else:
+                    char_df[-1].append(inp)
+                    flag = True
 
 
 class JobparserPhotosPipeline(ImagesPipeline):
@@ -35,11 +69,15 @@ class JobparserPhotosPipeline(ImagesPipeline):
 
 
     def item_completed(self, results, item, info):
-            item['photos'] = [itm[1] for itm in results if itm[0]]
-            return item
+        item['photos'] = [itm[1] for itm in results if itm[0]]
+
+        return item
 
     def file_path(self, request, response=None, info=None, *, item=None):
-        return str(item['_id']) + '/' + os.path.basename(urlparse(request.url).path)
+        image_url_hash = hashlib.shake_256(request.url.encode()).hexdigest(5)
+        title = item['title'].split(' ')[0]
+        image_filename = f'full/{title}/{image_url_hash}_{image_url_hash[3]}.jpg'
 
+        return image_filename
 
 
